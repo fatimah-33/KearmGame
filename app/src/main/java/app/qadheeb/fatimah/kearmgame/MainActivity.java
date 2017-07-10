eda1301c80493e4f2a3ed2d39da183c24e33ca18
Fatimah Mohammed <fmg1413@hotmail.com>
2017-07-10 18:35:38 +0300

    I have change the style on activity_main.xml I have correct the methods of add and subtract I have reorder the methods using Switch control

--- app/src/main/java/app/qadheeb/fatimah/kearmgame/MainActivity.java
+++ app/src/main/java/app/qadheeb/fatimah/kearmgame/MainActivity.java
@@ -3,155 +3,177 @@ package app.qadheeb.fatimah.kearmgame;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView textPlayer1;
-    TextView textPlayer2;
-    TextView textPlayer3;
-    TextView textPlayer4;
-    int player1 = 0;
-    int player2 = 0;
-    int player3 = 0;
-    int player4 = 0;
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView textPlayerOne;
+    TextView textPlayerTwo;
+    TextView textPlayerThree;
+    TextView textPlayerFour;
+    int scorePlayerOne = 0;
+    int scorePlayerTwo = 0;
+    int scorePlayerThree = 0;
+    int scorePlayerFour = 0;
+    Button five_points_player_one;
+    Button ten_points_player_one;
+    Button fifty_points_player_one;
+    Button sub_five_points_player_one;
+    Button five_points_player_two;
+    Button ten_points_player_two;
+    Button fifty_points_player_two;
+    Button sub_five_points_player_two;
+    Button five_points_player_three;
+    Button ten_points_player_three;
+    Button fifty_points_player_three;
+    Button sub_five_points_player_three;
+    Button five_points_player_four;
+    Button ten_points_player_four;
+    Button fifty_points_player_four;
+    Button sub_five_points_player_four;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textPlayer1 = (TextView) findViewById(R.id.s1);
-        textPlayer2 = (TextView) findViewById(R.id.s2);
-        textPlayer3 = (TextView) findViewById(R.id.s3);
-        textPlayer4 = (TextView) findViewById(R.id.s4);
+        textPlayerOne = (TextView) findViewById(R.id.score_for_player_one);
+        textPlayerTwo = (TextView) findViewById(R.id.score_for_player_two);
+        textPlayerThree = (TextView) findViewById(R.id.score_for_player_three);
+        textPlayerFour = (TextView) findViewById(R.id.score_for_player_four);
+        five_points_player_one = (Button) findViewById(R.id.five_points_player_one);
+        ten_points_player_one = (Button) findViewById(R.id.ten_points_player_one);
+        fifty_points_player_one = (Button) findViewById(R.id.fifty_points_player_one);
+        sub_five_points_player_one = (Button) findViewById(R.id.sub_five_points_player_one);
+        five_points_player_two = (Button) findViewById(R.id.five_points_player_two);
+        ten_points_player_two = (Button) findViewById(R.id.ten_points_player_two);
+        fifty_points_player_two = (Button) findViewById(R.id.fifty_points_player_two);
+        sub_five_points_player_two = (Button) findViewById(R.id.sub_five_points_player_two);
+        five_points_player_three = (Button) findViewById(R.id.five_points_player_three);
+        ten_points_player_three = (Button) findViewById(R.id.ten_points_player_three);
+        fifty_points_player_three = (Button) findViewById(R.id.fifty_points_player_three);
+        sub_five_points_player_three = (Button) findViewById(R.id.sub_five_points_player_three);
+        five_points_player_four = (Button) findViewById(R.id.five_points_player_four);
+        ten_points_player_four = (Button) findViewById(R.id.ten_points_player_four);
+        fifty_points_player_four = (Button) findViewById(R.id.fifty_points_player_four);
+        sub_five_points_player_four = (Button) findViewById(R.id.sub_five_points_player_four);
+        five_points_player_one.setOnClickListener(this);
+        ten_points_player_one.setOnClickListener(this);
+        fifty_points_player_one.setOnClickListener(this);
+        sub_five_points_player_one.setOnClickListener(this);
+        five_points_player_two.setOnClickListener(this);
+        ten_points_player_two.setOnClickListener(this);
+        fifty_points_player_two.setOnClickListener(this);
+        sub_five_points_player_two.setOnClickListener(this);
+        five_points_player_three.setOnClickListener(this);
+        ten_points_player_three.setOnClickListener(this);
+        fifty_points_player_three.setOnClickListener(this);
+        sub_five_points_player_three.setOnClickListener(this);
+        five_points_player_four.setOnClickListener(this);
+        ten_points_player_four.setOnClickListener(this);
+        fifty_points_player_four.setOnClickListener(this);
+        sub_five_points_player_four.setOnClickListener(this);
     }
 
-    //methods for player 1
-    public void plusFivePoints(View v) {
-        player1 = player1 + 5;
-        displayPlayer1(player1);
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.five_points_player_one:
+                scorePlayerOne += 5;
+                displayscorePlayerOne(scorePlayerOne);
+                break;
+            case R.id.ten_points_player_one:
+                scorePlayerOne += 10;
+                displayscorePlayerOne(scorePlayerOne);
+                break;
+            case R.id.fifty_points_player_one:
+                scorePlayerOne += 50;
+                displayscorePlayerOne(scorePlayerOne);
+                break;
+            case R.id.sub_five_points_player_one:
+                scorePlayerOne -= 5;
+                displayscorePlayerOne(scorePlayerOne);
+                break;
+            case R.id.five_points_player_two:
+                scorePlayerTwo += 5;
+                displayscorePlayerTwo(scorePlayerTwo);
+                break;
+            case R.id.ten_points_player_two:
+                scorePlayerTwo += 10;
+                displayscorePlayerTwo(scorePlayerTwo);
+                break;
+            case R.id.fifty_points_player_two:
+                scorePlayerTwo += 50;
+                displayscorePlayerTwo(scorePlayerTwo);
+                break;
+            case R.id.sub_five_points_player_two:
+                scorePlayerTwo -= 5;
+                displayscorePlayerTwo(scorePlayerTwo);
+                break;
+            case R.id.five_points_player_three:
+                scorePlayerThree += 5;
+                displayscorePlayerThree(scorePlayerThree);
+                break;
+            case R.id.ten_points_player_three:
+                scorePlayerThree += 10;
+                displayscorePlayerThree(scorePlayerThree);
+                break;
+            case R.id.fifty_points_player_three:
+                scorePlayerThree += 50;
+                displayscorePlayerThree(scorePlayerThree);
+                break;
+            case R.id.sub_five_points_player_three:
+                scorePlayerThree -= 5;
+                displayscorePlayerThree(scorePlayerThree);
+                break;
+            case R.id.five_points_player_four:
+                scorePlayerFour += 5;
+                displayscorePlayerFour(scorePlayerFour);
+                break;
+            case R.id.ten_points_player_four:
+                scorePlayerFour += 10;
+                displayscorePlayerFour(scorePlayerFour);
+                break;
+            case R.id.fifty_points_player_four:
+                scorePlayerFour += 50;
+                displayscorePlayerFour(scorePlayerFour);
+                break;
+            case R.id.sub_five_points_player_four:
+                scorePlayerFour -= 5;
+                displayscorePlayerFour(scorePlayerFour);
+                break;
+        }
     }
 
-    public void plusTenPoints(View v) {
-        player1 = player1 + 10;
-        displayPlayer1(player1);
-
+    //display score for player one
+    public void displayscorePlayerOne(int score) {
+        textPlayerOne.setText(String.valueOf(score));
     }
 
-    public void plusFivetyPoints(View v) {
-        player1 = player1 + 50;
-        displayPlayer1(player1);
-
+    //display score for player two
+    public void displayscorePlayerTwo(int score) {
+        textPlayerTwo.setText(String.valueOf(score));
     }
 
-    public void subFivePoints(View v) {
-        player1 = player1 - 5;
-        displayPlayer1(player1);
-
+    //display score for player three
+    public void displayscorePlayerThree(int score) {
+        textPlayerThree.setText(String.valueOf(score));
     }
 
-    //methods for player 2
-    public void plusFivePoints2(View v) {
-        player2 = player2 + 5;
-        displayPlayer2(player2);
+    //display score for player four
+    public void displayscorePlayerFour(int score) {
 
+        textPlayerFour.setText(String.valueOf(score));
     }
 
-    public void plusTenPoints2(View v) {
-        player2 = player2 + 10;
-        displayPlayer2(player2);
-
-    }
-
-    public void plusFivetyPoints2(View v) {
-        player2 = player2 + 50;
-        displayPlayer2(player2);
-
-    }
-
-    public void subFivePoints2(View v) {
-        player2 = player2 - 5;
-        displayPlayer2(player2);
-
-    }
-
-    //methods for player 3
-    public void plusFivePoints3(View v) {
-        player3 = player3 + 5;
-        displayPlayer3(player3);
-
-    }
-
-    public void plusTenPoints3(View v) {
-        player3 = player3 + 10;
-        displayPlayer3(player3);
-
-    }
-
-    public void plusFivetyPoints3(View v) {
-        player3 = player3 + 50;
-        displayPlayer3(player3);
-
-    }
-
-    public void subFivePoints3(View v) {
-        player3 = player3 - 5;
-        displayPlayer3(player3);
-
-    }
-
-    //methods for player 4
-    public void plusFivePoints4(View v) {
-        player4 = player4 + 5;
-        displayPlayer4(player4);
-
-    }
-
-    public void plusTenPoints4(View v) {
-        player4 = player4 + 10;
-        displayPlayer4(player4);
-
-    }
-
-    public void plusFivetyPoints4(View v) {
-        player4 = player4 + 50;
-        displayPlayer4(player4);
-
-    }
-
-    public void subFivePoints4(View v) {
-        player4 = player4 - 5;
-        displayPlayer4(player4);
-
-    }
-
-    //score for player 1
-    public void displayPlayer1(int score) {
-
-        textPlayer1.setText(String.valueOf(score));
-    }
-    //score for player 2
-
-    public void displayPlayer2(int score) {
-        textPlayer2.setText(String.valueOf(score));
-    }
-    //score for player 3
-
-    public void displayPlayer3(int score) {
-
-        textPlayer3.setText(String.valueOf(score));
-    }
-
-    //score for player 4
-    public void displayPlayer4(int score) {
-
-        textPlayer4.setText(String.valueOf(score));
-    }
-
-    //reset button
+    //reset button for all players
     public void resetButton(View v) {
-        textPlayer1.setText(String.valueOf(0));
-        textPlayer2.setText(String.valueOf(0));
-        textPlayer3.setText(String.valueOf(0));
-        textPlayer4.setText(String.valueOf(0));
+        scorePlayerOne = 0;
+        scorePlayerTwo = 0;
+        scorePlayerThree = 0;
+        scorePlayerFour = 0;
+        displayscorePlayerOne(scorePlayerOne);
+        displayscorePlayerTwo(scorePlayerTwo);
+        displayscorePlayerThree(scorePlayerThree);
+        displayscorePlayerFour(scorePlayerFour);
     }
 }
